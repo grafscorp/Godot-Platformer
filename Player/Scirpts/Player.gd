1b973cefdafbed37f0553e1fdfbc0b546e431b88
Falling Git <golyandringraf@gmail.com>
2023-02-01 18:41:20 +0300

    _jump

--- Player/Scirpts/Player.gd
+++ Player/Scirpts/Player.gd
@@ -8,16 +8,20 @@ export var gravity: float = 30.0
 var vec : Vector2 = Vector2.ZERO
 
 onready var sprite = $Sprite
-onready var coll = $Collision
+onready var coll:CollisionShape2D = $Collision
 
-func _process(delta):
-	vec.y += gravity
+func _process(delta)->void:
+	vec.y += gravity*delta
 	vec.x = Input.get_action_strength("d") - Input.get_action_strength("a")
-	if Input.is_action_just_pressed("space"):
-		vec.y -= jump
-	vec.x *=speed*delta
-	sprite.flip_h = vec.x <0
-func _physics_process(delta):
+
+	vec.x *=speed
+	if vec.x < 0:
+		sprite.flip_h = true
+	elif vec.x > 0:
+		sprite.flip_h = false
+func _physics_process(delta)->void:
 	#vec = move_and_slide_with_snap(vec*speed*delta, Vector2.UP,Vector2.ZERO,true)
 	vec = move_and_slide(vec,Vector2.UP)
-	vec = vec.normalized()
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		vec.y -= jump
+	#vec = vec.normalized()

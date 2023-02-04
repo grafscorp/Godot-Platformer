01c8d58115b0038de5070df14aad9e32e9b47ce6
Falling Git <golyandringraf@gmail.com>
2023-02-04 23:15:36 +0300

    straar

--- Scenes/Scripts/DemoLevel.gd
+++ Scenes/Scripts/DemoLevel.gd
@@ -1,5 +1,7 @@
 extends Node2D
-
+onready var player= preload("res://Player/Scene/Player.tscn")
+func _ready():
+	pass
 func _process(delta):
 	if Input.is_action_just_pressed("r"):
 		if get_tree().reload_current_scene() != OK:return
